75ce16a4f656796773c5b1929fc22de84d635c57
Marcel Härtel <7989230+BlizzCrafter@users.noreply.github.com>
2023-08-14 21:25:41 +0200

    Removed old, unused code.

--- MonoGame.Forms.NET/Services/GFXService.cs
+++ MonoGame.Forms.NET/Services/GFXService.cs
@@ -569,11 +569,9 @@ namespace MonoGame.Forms.NET.Services
             InternContent?.Dispose();
             ResourceContent?.Dispose();
             Pixel.Dispose();
-            Font = null;
-#if DX
             GetRenderTargetManager?.Dispose();
             RenderTargetTimer?.Dispose();
-#endif
+            Font = null;
         }
     }
 }
